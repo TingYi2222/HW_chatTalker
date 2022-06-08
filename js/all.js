420b431eb924e45decddb0cfb69884455ce8f761
TingYi2222 <noway520486@gmail.com>
2022-06-08 09:20:11 +0800

    更新

---
+++ js/all.js
@@ -0,0 +1,37 @@
+// 隱藏
+// $(document).ready(function () {
+//     $('h1').hide();
+// });
+
+//class用法
+// $(document).ready(function () {
+//     $('.title').hide();
+// });
+
+// id用法
+// $(document).ready(function () {
+//     $('#title').hide();
+// });
+
+// $(document).ready(function () {
+//     $('.item-link').click(function(e){
+//         e.preventDefault();
+//         $('.item-title').addClass('active'); // 注意：active 不用加上「.」，active為自己另設的style
+//     })  //點擊後所有item-title都會變。另外e.preventDefault()功用為防止產生預設行為，此地方為防止因click>a後，網址最後面多出#。
+
+    // $('.item-link').click(function(e){
+    //     e.preventDefault();
+    //     $(this).siblings().addClass('active'); // 注意：active 不用加上「.」
+    // })  //點擊後「同層」項會變化而已
+
+//     $('.item').click(function(e){
+//         e.preventDefault();
+//         $(this).find('a').addClass('active'); // 注意：active 不用加上「.」
+//     })  //點擊後「這個.item層內」的「所有a」會變化而已
+// });
+
+$(document).ready(function () {
+    $('.top-btn').click(function(e){
+        $('html').animate({scrollTop:0},200);
+    })
+});
